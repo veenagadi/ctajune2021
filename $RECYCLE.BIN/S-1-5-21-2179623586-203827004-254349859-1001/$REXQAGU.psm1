65564d410398ebfe8f8fa786896ace05543863eb
unknown <veena>
2021-07-13 17:14:27 +0530

    first commit

---
+++ $RECYCLE.BIN/S-1-5-21-2179623586-203827004-254349859-1001/$REXQAGU.psm1
@@ -0,0 +1,7 @@
+﻿func Get -Info
+{
+param($ComputerName)
+Get -Wmiobject -ComputerName $ComputerName -Class win32_BIOS
+}
+
+Export-ModuleMember -Function 'Get -Info'
---
+++ myfile/avg.ps1
@@ -0,0 +1,17 @@
+﻿$num1 = read-host "Enter the sub1"
+$num2 =read-host "Enter the sub2"
+$sum = $num1 +$num2
+$per= ($sum/200)*100
+clear-host
+if($per -ge 75)
+{
+"disinction "
+}
+elseif($per -ge 60 -and $per -lt 75)
+{
+"first class"
+}
+else
+{
+"third class"
+}
